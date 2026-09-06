8e92b40bb4cbd43aa1cd8dd6a974a60320c06f41
Fadet <tntboxmc@gmail.com>
2026-09-06 18:31:02 +0300

    minor fixes

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,14 +1,14 @@
 output "vm_external_ip" {
   description = "Kittygram VM public IP"
-  value       = yandex_vpc_address.infra_address.external_ipv4_address[0].address
+  value       = yandex_compute_instance.vm_1.network_interface[0].nat_ip_address
 }
 
 output "kittygram_url" {
   description = "Kittygram public URL"
-  value       = "http://${yandex_vpc_address.infra_address.external_ipv4_address[0].address}:${var.gateway_port}"
+  value       = "http://${yandex_compute_instance.vm_1.network_interface[0].nat_ip_address}:${var.gateway_port}"
 }
 
 output "ssh_command" {
   description = "SSH command for the deploy user"
-  value       = "ssh ${var.ssh_user}@${yandex_vpc_address.infra_address.external_ipv4_address[0].address}"
+  value       = "ssh ${var.ssh_user}@${yandex_compute_instance.vm_1.network_interface[0].nat_ip_address}"
 }
--- infra/vpc.tf
+++ infra/vpc.tf
@@ -10,14 +10,6 @@ resource "yandex_vpc_subnet" "infra_subnet" {
   network_id     = yandex_vpc_network.infra_network.id
 }
 
-resource "yandex_vpc_address" "infra_address" {
-  name = "kittygram-public-ip"
-
-  external_ipv4_address {
-    zone_id = var.zone
-  }
-}
-
 resource "yandex_vpc_security_group" "infra_sg" {
   name       = "${var.vpc_name}-sg"
   network_id = yandex_vpc_network.infra_network.id
